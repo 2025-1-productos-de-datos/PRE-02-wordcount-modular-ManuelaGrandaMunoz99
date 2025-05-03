be1590ebcbfc28e4828f18ff1407897f242edb7f
ManuelaGrandaMunoz99 <magrandam@unal.edu.co>
2025-05-03 11:57:26 -0500

    Prueba escritorio del test homework

---
+++ homework/__main__.py
@@ -0,0 +1,8 @@
+"""Entry point for the homework package."""
+
+# pytho -m homework data/input data/output
+
+from .src.wordcount import main
+
+if __name__ == "__main__":
+    main()
--- homework/src/_internals/read_all_lines.py
+++ homework/src/_internals/read_all_lines.py
@@ -1,12 +1,12 @@
 import os
 
-
-def read_all_lines():
+def read_all_lines(input_folder):
     all_lines = []
-    files_in_input_dir=os.listdir('data/input/')
+    files_in_input_dir=os.listdir(input_folder)
     for filename in files_in_input_dir:
-        file_path = os.path.join('data/input', filename)
+        file_path = os.path.join(input_folder, filename)
         with open(file_path,"r",encoding="utf-8") as f:
             lines = f.readlines()
             all_lines.extend(lines)
     return all_lines
+
